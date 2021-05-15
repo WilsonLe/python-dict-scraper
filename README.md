825d0c75e65e722ba11a827143f1d142962b7ef7
WilsonLe <leanhminh2907@gmail.com>
2021-05-15 16:38:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Create virtual env with:
+``python3 -m venv venv``
+or
+``python -m ven venv``
+
+Activate virtual env with:
+``venv\Scripts\activate.bat`` on Windows
+``source venv/bin/activate`` on MacOS
+
+Install dependencies with:
+``pip install -r requirements.txt``
+
+To deactivate virtual env:
+``deactivate``
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import requests
 import re
 from bs4 import BeautifulSoup
-from lxml import etree
 
 def scrape(word):
-	url = 'https://dictionary.cambridge.org/search/direct/?datasetsearch=english-vietnamese&q=good+time'
+	url = f'https://dictionary.cambridge.org/search/direct/?datasetsearch=english-vietnamese&q={word}'
 	header = {
 		"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1"
 	}
 	response = requests.get(url, headers=header)
 	content = response.content
 	soup = BeautifulSoup(content, 'html.parser')
-	dom = etree.HTML(str(soup))
-	text = dom.xpath('//*[@id="page-content"]/div[2]/div[1]/span[1]/div/div[3]/h2')[0].text
-	print(text)
+
+	query = soup.find("h2", {"class": "tw-bw dhw dpos-h_hw di-title"}).text
+	wordType = soup.find("span", {"class": "pos dpos"}).text
+	pronunciation = soup.find("span", {"class": "pron dpron"}).text
+
+	defBlocks = soup.find_all("div", {"class": "sense-block pr dsense dsense-noh"})
+	definitions = []
+	for defBlock in defBlocks:
+		textBlock = defBlock.text.strip() #3 lines of texts
+		texts = textBlock.split('\n') #split into 3 texts
+		texts[0] = texts[0][2:].strip() #trip the first
+		texts[1] = texts[1].strip() #trip the second
+		definitions.append({"en": texts[0], "vn": texts[1]})
+
+	return {
+		"query": query,
+		"wordType": wordType,
+		"pronunciation": pronunciation,
+		"definitions": definitions
+	}
 
 def main():
-	scrape('good')
+	data = scrape('nice')
+	for definition in data["definitions"]:
+		print(definition['vn'])
 
 main()
