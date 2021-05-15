a6075cf87866283b372696088d827f63118d1d2e
WilsonLe <leanhminh2907@gmail.com>
2021-05-15 16:51:42 -0400

    add documentation

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-def scrape(word):
+def cleanInput(input):
+	input = input.strip()
+	input = input.replace(' ', '-')
+	return input
+
+def sendRequest(word):
 	url = f'https://dictionary.cambridge.org/search/direct/?datasetsearch=english-vietnamese&q={word}'
 	header = {
 		"User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1"
 	}
 	response = requests.get(url, headers=header)
-	content = response.content
+	return response
+
+def extractContent(content):
 	soup = BeautifulSoup(content, 'html.parser')
 
 	query = soup.find("h2", {"class": "tw-bw dhw dpos-h_hw di-title"}).text
 	wordType = soup.find("span", {"class": "pos dpos"}).text
-	pronunciation = soup.find("span", {"class": "pron dpron"}).text
 
 	defBlocks = soup.find_all("div", {"class": "sense-block pr dsense dsense-noh"})
 	definitions = []
@@ -23,16 +29,36 @@ def scrape(word):
 		texts[0] = texts[0][2:].strip() #trip the first
 		texts[1] = texts[1].strip() #trip the second
 		definitions.append({"en": texts[0], "vn": texts[1]})
-
 	return {
 		"query": query,
 		"wordType": wordType,
-		"pronunciation": pronunciation,
 		"definitions": definitions
 	}
 
+def translate(word):
+	""" translate function with english input
+	return data: {
+		query: input word
+		wordType: type of input word (n., v., adj., ...)
+		definitions: [
+			{
+				en: english definition 1
+				vn: vietnamese definition 1
+			},
+			{
+				en: english definition 2
+				vn: vietnamese definition 2
+			},
+	}
+	"""
+	word = cleanInput(word)
+	response = sendRequest(word)
+	content = response.content
+	data = extractContent(content)
+	return data
+
 def main():
-	data = scrape('nice')
+	data = translate('funny')
 	for definition in data["definitions"]:
 		print(definition['vn'])
 
