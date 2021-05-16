adb4f725f8a397fc8e95b2062763e74cf213f55c
WilsonLe <leanhminh2907@gmail.com>
2021-05-16 17:12:03 -0400

    add multiple type search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import re
+from pprint import pprint
 from bs4 import BeautifulSoup
 
 def cleanInput(input):
@@ -16,50 +17,55 @@ def sendRequest(word):
 	return response
 
 def extractContent(content):
-	soup = BeautifulSoup(content, 'html.parser')
-
-	query = soup.find("h2", {"class": "tw-bw dhw dpos-h_hw di-title"}).text
-	wordType = soup.find("span", {"class": "pos dpos"}).text
-
-	defBlocks = soup.find_all("div", {"class": "sense-block pr dsense dsense-noh"})
-	definitions = []
-	for defBlock in defBlocks:
-		textBlock = defBlock.text.strip() #3 lines of texts
-		texts = textBlock.split('\n') #split into 3 texts
-		texts[0] = texts[0][2:].strip() #trip the first
-		texts[1] = texts[1].strip() #trip the second
-		definitions.append({"en": texts[0], "vn": texts[1]})
-	return {
-		"query": query,
-		"wordType": wordType,
-		"definitions": definitions
-	}
+	htmlSoup = BeautifulSoup(content, 'html.parser')
+	# this dictionary page top hierarchy is divided into word types (noun, verb, adj, interjection)
+	typeDOMs = htmlSoup.find_all("div", {"class": "d pr di english-vietnamese kdic"})
+	typeStrings = [*map(lambda dom: str(dom), typeDOMs)]
+	typeSoups = [*map(lambda dom: BeautifulSoup(dom, 'html.parser'), typeStrings)]
+	
+	types = []
+	for typeSoup in typeSoups:
+		query = typeSoup.find("h2", {"class": "tw-bw dhw dpos-h_hw di-title"}).text
+		wordType = typeSoup.find("span", {"class": "pos dpos"}).text
+		defBlocks = typeSoup.find_all("div", {"class": "sense-block pr dsense dsense-noh"})
+		definitions = []
+		for defBlock in defBlocks:
+			textBlock = defBlock.text.strip() #3 lines of texts
+			texts = textBlock.split('\n') #split into 3 texts
+			texts[0] = texts[0][2:].strip() #trip the first
+			texts[1] = texts[1].strip() #trip the second
+			definitions.append({"en": texts[0], "vn": texts[1]})
+		types.append({
+			"query": query,
+			"wordType": wordType,
+			"definitions": definitions
+		})
+	return types
 
 def translate(word):
 	""" translate function with english input
-	return data: {
-		query: input word
-		wordType: type of input word (n., v., adj., ...)
-		definitions: [
-			{
-				en: english definition 1
-				vn: vietnamese definition 1
-			},
-			{
-				en: english definition 2
-				vn: vietnamese definition 2
-			},
-	}
+	return data: [
+		{
+			query: input search
+			wordType: noun, adj, verb, interjection
+			definitions:[
+				{
+					en: english definition 1
+					vn: vietnamese definition 1
+				},...
+			]
+		},...
+	]
 	"""
 	word = cleanInput(word)
 	response = sendRequest(word)
-	content = response.content
+	content = response.text #.text or .content works fine with beautiful soup
 	data = extractContent(content)
 	return data
 
 def main():
-	data = translate('funny')
-	for definition in data["definitions"]:
-		print(definition['vn'])
+	data = translate('relative')
+	for wordType in data:
+		pprint(wordType)
 
 main()
